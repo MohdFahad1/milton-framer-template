ad25b1970c83d117f33e2c7c364d443f13d5ab96
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2023-12-02 11:01:01 +0530

    Navbar completed

---
+++ src/components/Navbar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import logo from '../assets/logo.png';
+import { FaBars } from "react-icons/fa6";
+
+const Navbar = () => {
+  return (
+    <nav className='py-4 px-6 bg-[#FDF2EC] border-b-[1px] flex justify-between items-center'>
+
+    {/* banner logo */}
+      <div>
+        <img src={logo} alt="logo" className="h-[45px]" />
+      </div>
+
+    {/*Links*/}
+      <div className='lg:flex hidden items-center gap-10'>
+
+      {/* Quick Links */}
+        <ul className='flex gap-10'>
+          <li className='cursor-pointer hover:text-purple-700 text-lg font-medium duration-300'>Features</li>
+          <li className='cursor-pointer hover:text-purple-700 text-lg font-medium duration-300'>FAQ</li>
+          <li className='cursor-pointer hover:text-purple-700 text-lg font-medium duration-300'>Pricing</li>
+          <li className='cursor-pointer hover:text-purple-700 text-lg font-medium duration-300'>Testimonials</li>
+        </ul>
+
+      {/* Button */}
+      <button className='bg-white border-[1px] hover:border-black duration-300 border-white p-3 rounded-xl text-lg font-bold'>
+        Buy Template
+      </button>
+      </div>
+
+      {/* responsive bars */}
+      <FaBars className='text-3xl lg:hidden block'/>
+    </nav>
+  )
+}
+
+export default Navbar
